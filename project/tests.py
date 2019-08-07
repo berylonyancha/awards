674bf779c81e4fac22663626fca2d67891df3427
Beryl Onyancha <berylonyancha@gmail>
2019-08-07 15:45:04 +0300

    Added tests for profile model

--- project/tests.py
+++ project/tests.py
@@ -15,4 +15,16 @@ class ProjectTestCase(TestCase):
         self.newproject.save()
         project = Project.objects.all()
         self.assertTrue(len(project)>0)
+class ProfileTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id =1,username='a')
+        self.new_profile = Profile(name=self.user, profile_pic='media/instagram/photo.jpg',bio='I am awesome')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_image(self):
+        self.new_profile.save()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
 
