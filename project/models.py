cf8acf981e3219694ff05ab1e1ee529c1441df9d
Beryl Onyancha <berylonyancha@gmail>
2019-08-07 15:55:16 +0300

    Add projects and its methods

--- project/models.py
+++ project/models.py
@@ -26,3 +26,31 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user__user__pk=id).first()
         return profile
+class Projects(models.Model):
+    project_photo = models.ImageField(upload_to='projects/')
+    profile = models.ForeignKey(Profile,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    url =models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now=True)
+    @classmethod
+    def all_projects(cls):
+        projects = cls.objects.all()
+        return projects
+    @classmethod
+    def get_profile_projects(cls, username):
+        userprojects = Projects.objects.filter(profile__pk=username)
+        return userprojects
+    @classmethod
+    def days_projects(cls,date):
+        projects = cls.objects.filter(pub_date__date = date)
+        return projects
+    @classmethod
+    def search_by_title(cls,search_term):
+        allprojects = cls.objects.filter(title__icontains=search_term)
+        return allprojects
+
+    
+
+    
